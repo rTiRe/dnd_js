3d160c832d7a8bc12e352fc1b7bf3b46694809e7
slavikyd <slavikyd@gmail.com>
2024-10-17 23:16:57 +0300

    ~

--- fight.js
+++ fight.js
@@ -3,37 +3,48 @@ function random_list(list){
     return list[random];
 }
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 function handleFight(player, monster){
     aborter.abort();
     while (player.health > 0 || monster.health > 0){
         console.log(`==== ${monster.name} =====`)
         console.log(random_list(monster.appearance_messages))
-        if (Math.random(0, 1) >= monster.attack_chance) {
-            player.health -= monster.damage
-            msg = random_list(monster.success_attack_messages)
-            console.log(msg)
+        console.log(`Что делать? (1 - атаковать, 0 - открыть инвентарь)`)
+        if (rl.question(`\n>>> Ваш выбор: `, { signal: aborter.signal }, (answer) => {}) === 0)
+        {
+            if (Math.random(0, 1) >= monster.attack_chance) {
+                player.health -= monster.damage
+                msg = random_list(monster.success_attack_messages)
+                console.log(msg)
+            }
+            else {
+                player.health -= monster.damage
+                msg = random_list(monster.miss_attack_messages)
+                console.log(msg)
+            }
+            console.log(`Ваша очередь!`)
+            if (Math.random(0, 1) <= player.weapon.attack_chance){
+                console.log(`Вы попали!`)
+                monster.health -= player.weapon.damage
+            }
+            else if (monster.health <= 0){
+                console.log(`Вы одолели монстра!`)
+                break
+            }
+            else if (player.health <= 0){
+                console.log(`Гаме овер!`)
+                break
+            }
+            else {
+                console.log(`Мимо!`)
+            }
         }
         else {
-            player.health -= monster.damage
-            msg = random_list(monster.miss_attack_messages)
-            console.log(msg)
-        }
-        console.log(`Ваша очередь!`)
-        if (Math.random(0, 1) <= player.weapon.attack_chance){
-            console.log(`Вы попали!`)
-            monster.health -= player.weapon.damage
-        }
-        else if (monster.health <= 0){
-            console.log(`Вы одолели монстра!`)
-            break
-        }
-        else if (player.health <= 0){
-            console.log(`Гаме овер!`)
-            break
-        }
-        else {
-            console.log(`Мимо!`)
+            //
         }
     }
 }
